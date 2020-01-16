d58fd03f41292c185fbf93f1cdf9f4781585d0ac
Florian Barth <barth@fmi.uni-stuttgart.de>
2020-01-16 15:13:38 +0100

    Add new visualization style for non opt path segments

--- src/bin/visualize-trajectory.rs
+++ src/bin/visualize-trajectory.rs
@@ -13,6 +13,7 @@ use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Opt {
+    /// Style of visualization
     #[structopt(subcommand)]
     style: Style,
     /// File containing all trajectories
@@ -65,7 +66,7 @@ fn main() -> MyResult<()> {
 
     let feature_collection = match style {
         Style::Cuts => visualize_cuts(&trajectory, &splitting, &geojson_map),
-        Style::NonOpts => todo!("visualize snosp"),
+        Style::NonOpts => visualize_snops(&trajectory, &splitting, &geojson_map),
     };
 
     let outfile = format!("geojson_trajectory_{}.json", trajectory_id);
@@ -146,6 +147,117 @@ fn create_cut_properties(
         Value::Number(Number::from_f64(5.0).unwrap()),
     );
     map.insert("preference".to_owned(), Value::String(format!("{:?}", p)));
+
+    Some(map)
+}
+
+fn visualize_snops(
+    trajectory: &Trajectory,
+    splitting: &SplittingStatistics,
+    geojson_map: &HashMap<i64, Geometry>,
+) -> FeatureCollection {
+    let mut last = 0;
+    let mut features: Vec<_> = splitting
+        .non_opt_subpaths
+        .as_ref()
+        .expect("SNOPs data not present in Results file")
+        .non_opt_subpaths
+        .iter()
+        .flat_map(|(start, end)| {
+            let line_strings = trajectory.path[last..*start]
+                .iter()
+                .map(|e| geojson_map[e].clone())
+                .collect();
+
+            let geo_collection = Value::GeometryCollection(line_strings);
+            let geometry = Geometry {
+                bbox: None,
+                foreign_members: None,
+                value: geo_collection,
+            };
+
+            let opt = Feature {
+                id: None,
+                bbox: None,
+                foreign_members: None,
+                properties: create_non_opt_properties(false),
+                geometry: Some(geometry),
+            };
+
+            let line_strings = trajectory.path[*start..*end]
+                .iter()
+                .map(|e| geojson_map[e].clone())
+                .collect();
+
+            let geo_collection = Value::GeometryCollection(line_strings);
+            let geometry = Geometry {
+                bbox: None,
+                foreign_members: None,
+                value: geo_collection,
+            };
+
+            let non_opt = Feature {
+                id: None,
+                bbox: None,
+                foreign_members: None,
+                properties: create_non_opt_properties(true),
+                geometry: Some(geometry),
+            };
+
+            last = *end;
+
+            vec![opt, non_opt]
+        })
+        .collect();
+
+    let line_strings = trajectory.path[last as usize..trajectory.path.len()]
+        .iter()
+        .map(|e| geojson_map[e].clone())
+        .collect();
+
+    let geo_collection = Value::GeometryCollection(line_strings);
+    let geometry = Geometry {
+        bbox: None,
+        foreign_members: None,
+        value: geo_collection,
+    };
+
+    let opt = Feature {
+        id: None,
+        bbox: None,
+        foreign_members: None,
+        properties: create_non_opt_properties(false),
+        geometry: Some(geometry),
+    };
+
+    features.push(opt);
+
+    FeatureCollection {
+        bbox: None,
+        features,
+        foreign_members: None,
+    }
+}
+
+fn create_non_opt_properties(
+    non_opt: bool,
+) -> Option<serde_json::map::Map<String, serde_json::Value>> {
+    use serde_json::{Number, Value};
+    let mut map = serde_json::map::Map::new();
+
+    let color = if non_opt { "#ff0000" } else { "#000000" };
+
+    map.insert("stroke".to_owned(), Value::String(color.to_owned()));
+
+    map.insert(
+        "stroke-opacity".to_owned(),
+        Value::Number(Number::from_f64(0.5).unwrap()),
+    );
+
+    map.insert(
+        "stroke-width".to_owned(),
+        Value::Number(Number::from_f64(5.0).unwrap()),
+    );
 
     Some(map)
 }
