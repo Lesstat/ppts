a96396639d677562ea28a8d32a7edd7cda694c25
Florian Barth <florianbarth@gmx.de>
2020-02-05 11:53:27 +0100

    Run rustfmt and delete unnecessary file

--- src/graph/trajectory_analysis.rs
+++ src/graph/trajectory_analysis.rs
@@ -223,105 +223,104 @@ mod tests {
         assert_eq!(2, non_opts[1].start_index);
         assert_eq!(4, non_opts[1].end_index);
     }
-	
-	/*
-	Takes the optimal path cost vectors for each metric (e.g. alpha = (1,0,0)) and the cost vector of the actual path. 
-	Returns a scalar vector alpha that is an approximation of the underlying preferences of the actual path. 
-	If there are multiple paths with the same preference the input should be the sum of the paths' costs (both for the actual costs and the optimal costs).
-	
-	costs_per_metric: vector of scalar vectors, each should be the costs of the optimal path for a single metric
-	real_costs: the cost vector of the path for which the preferences should be computed
-	return value: approximation of the underlying preferences
-	*/
-	fn get_linear_combination(costs_per_metric: &Vec<Vec<f64>>, real_costs: &Vec<f64>) -> Vec<f64>{
-		let mut finished : bool = false;
-		let dim: usize = real_costs.len();
-		let mut alpha : Vec<f64> = vec![0.0; dim];
-		let mut rest : Vec<f64> = real_costs.to_vec();
-		let mut distance : f64 = get_length(&real_costs);
-		let mut normalized_costs_per_metric : Vec<Vec<f64>> = Vec::new();
-		for i in 0..dim{
-			normalized_costs_per_metric.push(normalize_vec(&costs_per_metric[i]));
-		}
-		let mut count : usize = 0;
-		while !finished {
-			let mut best_scalar : f64 = 0.0;
-			let mut best_index : usize = 0;
-			for i in 0..dim{
-				let mut scalar : f64 = get_scalar_product(&normalized_costs_per_metric[i], &rest);
-				if scalar + alpha[i] < 0.0{
-					scalar = -alpha[i];
-				}
-				if scalar.abs() > best_scalar.abs() {
-					best_scalar = scalar;
-					best_index = i;
-				}
-			}
-			let best_metric : &Vec<f64> = &normalized_costs_per_metric[best_index];
-			let mut step : Vec<f64> = vec![0.0;dim];
-			for i in 0..dim{
-				step[i] = best_scalar*best_metric[i];
-				rest[i] -= step[i];
-			}
-			let dist : f64 = get_length(&step);
-			if dist < 0.000001{
-				finished = true;
-			}else{
-				alpha[best_index] += best_scalar;
-				distance = dist;
-			}
-			count += 1;
-		}
-		let mut sum_alpha : f64 = 0.0;
-		for i in 0..dim{
-			sum_alpha += alpha[i];
-		}
-		alpha.iter_mut()
-		.for_each(|x| *x /= sum_alpha);
-		return alpha;
-	}
 
-	/*
-	helper function for get_linear_combination
-	*/
-	fn get_scalar_product(vec1: &Vec<f64>, vec2: &Vec<f64>) -> f64{
-		let mut res: f64 = 0.0;
-		let dim: usize = vec1.len();
-		for i in 0..dim{
-			res += vec1[i]*vec2[i];
-		}	
-		return res;
-	}
+    /*
+    Takes the optimal path cost vectors for each metric (e.g. alpha = (1,0,0)) and the cost vector of the actual path.
+    Returns a scalar vector alpha that is an approximation of the underlying preferences of the actual path.
+    If there are multiple paths with the same preference the input should be the sum of the paths' costs (both for the actual costs and the optimal costs).
 
-	/*
-	helper function for get_linear_combination
-	*/
-	fn normalize_vec(vec: &Vec<f64>) -> Vec<f64>{
-		let mut res : Vec<f64> = vec![0.0; vec.len()];
-		let length : f64 = get_length(&vec);
-		for i in 0..vec.len(){
-			res[i] = vec[i] / length;
-		}
-		return res;
-	}
+    costs_per_metric: vector of scalar vectors, each should be the costs of the optimal path for a single metric
+    real_costs: the cost vector of the path for which the preferences should be computed
+    return value: approximation of the underlying preferences
+     */
+    fn get_linear_combination(costs_per_metric: &Vec<Vec<f64>>, real_costs: &Vec<f64>) -> Vec<f64> {
+        let mut finished: bool = false;
+        let dim: usize = real_costs.len();
+        let mut alpha: Vec<f64> = vec![0.0; dim];
+        let mut rest: Vec<f64> = real_costs.to_vec();
+        let mut distance: f64 = get_length(&real_costs);
+        let mut normalized_costs_per_metric: Vec<Vec<f64>> = Vec::new();
+        for i in 0..dim {
+            normalized_costs_per_metric.push(normalize_vec(&costs_per_metric[i]));
+        }
+        let mut count: usize = 0;
+        while !finished {
+            let mut best_scalar: f64 = 0.0;
+            let mut best_index: usize = 0;
+            for i in 0..dim {
+                let mut scalar: f64 = get_scalar_product(&normalized_costs_per_metric[i], &rest);
+                if scalar + alpha[i] < 0.0 {
+                    scalar = -alpha[i];
+                }
+                if scalar.abs() > best_scalar.abs() {
+                    best_scalar = scalar;
+                    best_index = i;
+                }
+            }
+            let best_metric: &Vec<f64> = &normalized_costs_per_metric[best_index];
+            let mut step: Vec<f64> = vec![0.0; dim];
+            for i in 0..dim {
+                step[i] = best_scalar * best_metric[i];
+                rest[i] -= step[i];
+            }
+            let dist: f64 = get_length(&step);
+            if dist < 0.000001 {
+                finished = true;
+            } else {
+                alpha[best_index] += best_scalar;
+                distance = dist;
+            }
+            count += 1;
+        }
+        let mut sum_alpha: f64 = 0.0;
+        for i in 0..dim {
+            sum_alpha += alpha[i];
+        }
+        alpha.iter_mut().for_each(|x| *x /= sum_alpha);
+        return alpha;
+    }
 
-	/* 
-	helper function for get_linear_combination
-	*/
-	fn get_distance(vec1: &Vec<f64>, vec2: &Vec<f64>) -> f64{
-		let mut res: f64 = 0.0;
-		for i in 0..vec1.len(){
-			let dif : f64 = vec1[i] - vec2[i];
-			res += dif*dif;
-		}
-		return res.sqrt();
-	}
-	
-	/* 
-	helper function for get_linear_combination
-	*/
-	fn get_length(vec: &Vec<f64>) -> f64{
-		let zeros = vec![0.0; vec.len()];
-		return get_distance(&vec, &zeros);
-	}
+    /*
+    helper function for get_linear_combination
+     */
+    fn get_scalar_product(vec1: &Vec<f64>, vec2: &Vec<f64>) -> f64 {
+        let mut res: f64 = 0.0;
+        let dim: usize = vec1.len();
+        for i in 0..dim {
+            res += vec1[i] * vec2[i];
+        }
+        return res;
+    }
+
+    /*
+    helper function for get_linear_combination
+     */
+    fn normalize_vec(vec: &Vec<f64>) -> Vec<f64> {
+        let mut res: Vec<f64> = vec![0.0; vec.len()];
+        let length: f64 = get_length(&vec);
+        for i in 0..vec.len() {
+            res[i] = vec[i] / length;
+        }
+        return res;
+    }
+
+    /*
+    helper function for get_linear_combination
+     */
+    fn get_distance(vec1: &Vec<f64>, vec2: &Vec<f64>) -> f64 {
+        let mut res: f64 = 0.0;
+        for i in 0..vec1.len() {
+            let dif: f64 = vec1[i] - vec2[i];
+            res += dif * dif;
+        }
+        return res.sqrt();
+    }
+
+    /*
+    helper function for get_linear_combination
+     */
+    fn get_length(vec: &Vec<f64>) -> f64 {
+        let zeros = vec![0.0; vec.len()];
+        return get_distance(&vec, &zeros);
+    }
 }
